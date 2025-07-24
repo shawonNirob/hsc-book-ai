74e936ab535890e1eb61d22997cc2d2f958f0d71
shawonNirob <shawonnirob16@gmail.com>
2025-07-25 01:15:12 +0600

    qdrant client setup

---
+++ app/qdrant/qdrant_connect.py
@@ -0,0 +1,7 @@
+from qdrant_client import QdrantClient
+from app.config import settings
+
+client = QdrantClient(
+    url="https://2a964a00-5b72-4b2c-84b2-9afb20f59a30.us-east-1-0.aws.cloud.qdrant.io:6333", 
+    api_key=settings.QDRANT_API_KEY,
+)
